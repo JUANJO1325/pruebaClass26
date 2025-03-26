8eda285d6fe4d8e86a064f3f453853dc94576717
Dokarii <ricardotejedor18@gmail.com>
2025-03-26 10:18:39 -0500

    Actualizando main con calculadora

---
+++ main.py
@@ -0,0 +1,20 @@
+numero1=()
+numero2=()
+
+suma= numero1 + numero2
+
+input("Suma de numeros: " numero1 + "Numero dos" + numero2)
+
+print("Resultado de la suma: ", suma)
+
+
+# number1=1
+# number2=2
+# suma = number1 + number2
+# multiplicacion = number1 + number2
+
+
+
+# print("Suma: ", suma)
+# print("Multiplicacion: ", multiplicacion)
+
