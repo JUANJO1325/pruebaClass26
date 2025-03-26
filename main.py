70fef175b74d6614a061cb88428ca957af954366
Dokarii <ricardotejedor18@gmail.com>
2025-03-26 10:20:38 -0500

    chore: actualizacion con nuevos datos

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ suma= numero1 + numero2
 input("Suma de numeros: " numero1 + "Numero dos" + numero2)
 
 print("Resultado de la suma: ", suma)
+print("Resultado de la suma: ", suma)
+print("Resultado de la suma: ", suma)
 
 
 # number1=1
